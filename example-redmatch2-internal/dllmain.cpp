e6a8af453618d44bfc69a8829ac18a06a1ae0787
tikkamasala <github.crewmate584@passinbox.com>
2025-04-30 17:11:48 +0200

    Split cheat functions into 2 files that includes il2cpp_resolver.hpp

--- example-redmatch2-internal/dllmain.cpp
+++ example-redmatch2-internal/dllmain.cpp
@@ -9,6 +9,7 @@
 #include <xenon/components/features/aimbot.hpp>
 #include <xenon/models/waypoint.hpp>
 #include <il2cpp_resolver/il2cpp_resolver.hpp>
+#include <lua/lua.hpp>
 
 #include "RedMatch2Dump/il2cpp.h"
 
@@ -16,32 +17,7 @@ float head = 0.8f;
 float feet = -0.8f;
 float width = 300.0f;
 
-bool WorldToScreen(Vec2 screenSize, Vec3 world, Vec2& screen)
-{
-	Unity::CCamera* cameraMain = Unity::Camera::GetMain();
-	if (!cameraMain) {
-		return false;
-	}
-
-	Unity::Vector3 buffer = cameraMain->CallMethodSafe<Unity::Vector3>("WorldToScreenPoint", Unity::Vector3(world.x, world.y, world.z), Unity::m_eCameraEye::m_eCameraEye_Center);
-
-	if (buffer.x > screenSize.x || buffer.y > screenSize.y || buffer.x < 0 || buffer.y < 0 || buffer.z < 0)
-	{
-		return false;
-	}
-
-	if (buffer.z > 0.0f)
-	{
-		screen = Vec2(buffer.x, screenSize.y - buffer.y);
-	}
-
-	if (screen.x > 0 || screen.y > 0)
-	{
-		return true;
-	}
-
-	return false;
-}
+bool WorldToScreen(Vec2 screenSize, Vec3 world, Vec2& screen);
 
 DWORD WINAPI MainThread(LPVOID lpReserved)
 {
